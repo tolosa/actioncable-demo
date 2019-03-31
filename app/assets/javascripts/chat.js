fdd87c59791b357ff324bc3d015667a6185d6439
Leonardo Tolosa <tolosa@gmail.com>
2019-03-31 14:05:04 -0300

    Add send button; refactor JS

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -1,10 +1,28 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-$(document).on('keypress', '#message_field', event => {
-  if(event.keyCode == 13) {
-    App.chat.speak(event.target.value);
-    event.target.value = '';
-    event.preventDefault();
+$(document).ready(function() {
+  const $field = $('#message_field');
+  const $btnSend = $('#btn-send');
+
+  function sendTypedMessage() {
+    App.chat.speak($field.val());
+    $field.val('');
+    updateSendButtonStatus();
   }
+
+  function updateSendButtonStatus() {
+    $btnSend.prop('disabled', $field.val().length == 0);
+  }
+
+  $field
+    .on('input', updateSendButtonStatus)
+    .on('keypress', event => {
+      if(event.keyCode == 13) {
+        sendTypedMessage();
+        event.preventDefault();
+      }
+    });
+
+  $btnSend.click(sendTypedMessage);
 });
