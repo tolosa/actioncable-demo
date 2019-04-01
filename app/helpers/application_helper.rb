d461129b0dd408867ea21ddfa181b4a4fe444737
Leonardo Tolosa <tolosa@gmail.com>
2019-04-01 20:54:36 -0300

    Move avatar generation to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def avatar_image(username, size: '50x50')
+    image_tag "//robohash.org/#{URI.encode(username)}?set=set4&bgset=bg2", size: size
+  end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,3 @@
 class Message < ApplicationRecord
   validates :text, :username, presence: true
-
-  def avatar(size: '50x50')
-    "https://robohash.org/#{URI.encode(username)}?set=set4&bgset=bg2"
-  end
 end
