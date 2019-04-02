af48768dbcb007ef1636ab6728bfc536b566322d
Leonardo Tolosa <tolosa@gmail.com>
2019-04-02 16:08:09 -0300

    Move chat JS code to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,29 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function() {
+  const $field = $('#message_field');
+  const $btnSend = $('#btn-send');
+
+  function sendTypedMessage() {
+    App.chat.speak($field.val());
+    $field.val('');
+    updateSendButtonStatus();
+  }
+
+  function updateSendButtonStatus() {
+    $btnSend.prop('disabled', $field.val().length == 0);
+  }
+
+  $field
+    .on('input', updateSendButtonStatus)
+    .on('keypress', event => {
+      if(event.keyCode == 13) {
+        sendTypedMessage();
+        event.preventDefault();
+      }
+    });
+
+  $btnSend.click(sendTypedMessage);
+});
