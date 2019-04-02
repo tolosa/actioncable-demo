5fe125f7cf3cdbe45ac1f64cc504424d2cbbb54e
Leonardo Tolosa <tolosa@gmail.com>
2019-04-01 21:04:38 -0300

    Refactor size parameter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def avatar_image(username, size: '50x50')
-    image_tag "//robohash.org/#{URI.encode(username)}?set=set4&bgset=bg2", size: size
+  def avatar_image(username, size: 50)
+    image_tag "//robohash.org/#{URI.encode(username)}?set=set4&bgset=bg2", width: size, height: size
   end
 end
